a15eccbf7c074b27051f0d84e303101252ee5375
facundo2309 <facundoomargarcia@gmail.com>
2021-11-22 17:46:04 -0300

    prueba controllers

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,24 @@
 const { Router } = require('express');
+
+var express=require('express')
+
+const { home } = require('../controllers/controllers');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
+
+
 const router = Router();
+
+router.use(express.json())
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/home',home)
+
 
 
 module.exports = router;
