334deb440261c952b5738c080c05f25cfa201025
facundo2309 <facundoomargarcia@gmail.com>
2021-11-22 11:34:45 -0300

    creacion modelo base de datos tablas

---
+++ api/src/models/Activity.js
@@ -0,0 +1,44 @@
+const {DataTypes} = require('sequelize'); 
+
+module.exports=(sequelize)=>{
+
+    sequelize.define('activity',{
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull:false
+
+        },
+
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+
+        difficulty:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            defaultValue:1
+
+
+        },
+
+        duration:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            defaultValue:'not specified'
+
+
+        },
+
+        season:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            defaultValue:'not specified'
+        }
+
+
+
+
+    })
+}
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,49 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+
+    id:{
+      type:DataTypes.STRING,
+      primaryKey: true,
+      allowNull:false
+
+    },
+
+
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    image:{
+      type:DataTypes.STRING,
+      defaultValue:null
+    },
+
+    continent:{
+      type:DataTypes.STRING,
+      defaultValue:null
+    },
+
+    capital:{
+      type:DataTypes.STRING,
+      defaultValue:null
+    },
+
+    subregion:{
+      type:DataTypes.STRING,
+      defaultValue:null
+    },
+
+    area:{
+      type:DataTypes.INTEGER,
+      defaultValue:null
+    },
+
+    population:{
+      type:DataTypes.INTEGER,
+      defaultValue:null
+    }
   });
 };
