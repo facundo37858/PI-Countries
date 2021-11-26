df3cef8bc147e25d1cb8c88d40ea3f7c9644537e
facundo2309 <facundoomargarcia@gmail.com>
2021-11-26 17:14:17 -0300

    render home

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+
 import './App.css';
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <Home></Home>
     </div>
   );
 }
