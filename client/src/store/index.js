590910098b0d95cf4a30e07d8bbe5f42df260b11
facundo2309 <facundoomargarcia@gmail.com>
2021-11-26 17:15:33 -0300

    store

---
+++ client/src/store/index.js
@@ -0,0 +1,16 @@
+
+import {createStore, applyMiddleware,compose } from 'redux';
+
+import rootReducer from '../reducer/index.js';
+
+import thunk from 'redux-thunk';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store= createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store
+
