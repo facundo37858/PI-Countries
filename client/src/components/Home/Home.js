6c7990f38c020056564b156ab871da52c390218a
facundo2309 <facundoomargarcia@gmail.com>
2021-11-28 18:51:31 -0300

    cundo no encuentro el pais

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,12 +4,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCountries } from "../../actions";
 import Card from "../Card/Card";
+import NavBar from "../NavBar/NavBar";
 
 
 
 export default function Home(){
 
     let countriesAll=useSelector(state=>state.countries)
+
+    
 
     let dispach=useDispatch()
 
@@ -21,9 +24,16 @@ export default function Home(){
 
 
     return(
-        <div>
 
-           {countriesAll.map(country=>{return(<Card name={country.name} image={country.image} continent={country.continent}></Card>)})} 
+        <div>
+            <div>
+                <NavBar></NavBar>
+            </div>
+
+           {Array.isArray(countriesAll)?countriesAll.map(country=>{return(<Card key={country.id} name={country.name} image={country.image} continent={country.continent}></Card>)})
+           :
+           <p>{countriesAll}</p>
+           } 
 
         </div>
 
