0785d8f26e957e5d26b2fe90e80b9bbfe61782b4
facundo2309 <facundoomargarcia@gmail.com>
2021-11-28 18:53:31 -0300

    filters

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,9 +1,15 @@
 
-import { GET_COUNTRIES } from "../actions/constants";
+import { GET_COUNTRIES, GET_COUNTRY_BY_NAME, FILTER_BY_CONTINENT, FILTER_BY_ACTIVITY} from "../actions/constants";
 
 const initialState={
 
-    countries:[]
+    countriesBackup:[],
+
+    countries:[],
+
+    countriesByContinent:[],
+
+    countriesByActivity:[]
 
 }
 
@@ -14,9 +20,28 @@ export default function rootReducer(state=initialState,action){
             
             return{
                 ...state,
+                countriesBackup:action.payload,
                 countries:action.payload
                 
             };
+        case GET_COUNTRY_BY_NAME:
+            return{
+                ...state,
+                countries:action.payload
+            }
+        case FILTER_BY_CONTINENT:
+            return{
+                ...state,
+                countries:state.countriesByContinent
+
+            }
+        case FILTER_BY_ACTIVITY:
+            return{
+                ...state,
+                countries:state.countriesByActivity
+            }
+
+
     
         default:
             return state;
