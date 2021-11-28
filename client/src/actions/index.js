c22cd0a6af74307ab4c0b641f98a58cb9be67433
facundo2309 <facundoomargarcia@gmail.com>
2021-11-28 18:50:03 -0300

    search filters

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 
 
-import { GET_COUNTRIES } from "./constants";
+import { GET_COUNTRIES, GET_COUNTRY_BY_NAME, FILTER_BY_CONTINENT , FILTER_BY_ACTIVITY} from "./constants";
 
 import axios from 'axios'
 
@@ -17,6 +17,45 @@ export function getCountries(){
         return dispach({
             type:GET_COUNTRIES,
             payload:countries
+        })
+    }
+
+
+}
+
+export function searchCountry(name){
+    return async function(dispach){
+
+        let countryResult=await axios.get(`http://localhost:3001/countries?name=${name}`)
+        .then((json)=>{return json.data})
+
+        return dispach({
+            type:GET_COUNTRY_BY_NAME,
+            payload:countryResult
+        })
+
+
+    }
+}
+
+export  function filterByContinent(continent){
+
+    return function(dispach){
+        return dispach({
+            type:FILTER_BY_CONTINENT,
+            payload:continent
+        })
+    }
+
+
+}
+
+export  function FilterByActivity(activity){
+
+    return function(dispach){
+        return dispach({
+            type:FILTER_BY_ACTIVITY,
+            payload:activity
         })
     }
 
