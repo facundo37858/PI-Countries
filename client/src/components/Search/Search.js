e043a93d02e5ebe154ff99a1c789443d7e01e6b3
facundo2309 <facundoomargarcia@gmail.com>
2021-11-28 18:53:17 -0300

    Search

---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { searchCountry } from "../../actions";
+
+
+export default function Search(){
+
+    //let countrySearch=useSelector(state=>state.countries)
+
+    let dispach=useDispatch()
+
+    const [country,setCountry]=useState('')
+
+    const handleSubmit=(event)=>{
+        event.preventDefault()
+        dispach(searchCountry(country))
+        setCountry('')
+
+
+
+    }
+
+    const handleChange=(event)=>{
+        event.preventDefault()
+        setCountry(event.target.value)
+        
+    }
+
+    const clearSearch=(e)=>{
+        e.preventDefault()
+        dispach(searchCountry(country))
+        setCountry('')
+
+    }
+
+
+    return(
+        <div>
+            <form onSubmit={(e)=>{handleSubmit(e)}}>
+
+                <input 
+                type='text'
+                id='country' 
+                autoComplete='off' 
+                value={country} 
+                placeholder='find country...'
+                onChange={(e)=>handleChange(e)}/>
+
+                <button type='submit' to='/home/search'>Search</button> 
+                <button onClick={(e)=>clearSearch(e)}>Clear Search</button>
+
+
+            </form>
+           
+        </div>
+    )
+}
