23d177899f295fa99f468fb922d6b6281f4da14e
facundo2309 <facundoomargarcia@gmail.com>
2021-11-28 18:50:18 -0300

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 
+import { Route, Switch } from 'react-router';
 import './App.css';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Switch>
+
+        <Route exact path='/' component={Login}></Route>
+        <Route path='/home' component={Home}></Route>
+      
+      </Switch>
     </div>
   );
 }
