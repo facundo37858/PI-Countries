618a0c589d9b3c009ff380ed9aed0b6a5ec9d325
facundo2309 <facundoomargarcia@gmail.com>
2021-12-01 19:47:10 -0300

    reac order az za

---
+++ client/src/components/Order/Order.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { order } from "../../actions";
+
+
+export default function Order(){
+
+    
+
+    let dispach=useDispatch()
+
+    const handelOrder=(e)=>{
+        e.preventDefault()
+
+        dispach(order(e.target.value))
+    }
+
+
+     
+    return(
+        <div>
+            <p><label htmlFor='select-order'>Order:</label></p>
+
+            <select name='order' onChange={(e)=>handelOrder(e)}>
+                <option value='ANY'>--Please choose an option--</option>
+                <option value='ASC'>A-Z</option>
+                <option value='DESC'>Z-A</option>
+
+            </select>
+        </div>
+    )
+}
+
+
+// {/* <p><label htmlFor='activity-select'>Filter by Activity</label></p>
+
+// <select name='activity' onChange={(e)=>handelFilter(e)}>
+//     {/* <option value='all'>--Please choose an option--</option> */}
+//     <option value='all'>Show All</option>
+
+//     {notRepitActivitiest.length>0&& notRepitActivitiest.map((activity,index)=>{
+
+//         return(<option key={index} value={activity}>{activity}</option>)})
+//     }    
+        
+   
+
+
+    
+
+// </select> */}
