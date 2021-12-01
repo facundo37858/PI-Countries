2ae7d767f5579a00e957c8dba68834e408ebca86
facundo2309 <facundoomargarcia@gmail.com>
2021-12-01 19:46:45 -0300

    order nav bar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import FilterByActivity from "../FilterByActivity/FilterByActivity";
 import FilterByContinent from "../FilterByContinent/FilterByContinent";
+import Order from "../Order/Order";
 import Search from "../Search/Search";
 
 import style from './style.module.css'
@@ -18,6 +19,7 @@ export default function NavBar(){
             <Link to='/home'><div className={style.home}>Home</div></Link>
             
             <Search></Search>
+            <Order/>
 
             <FilterByContinent></FilterByContinent>
             <FilterByActivity></FilterByActivity>
