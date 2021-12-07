08fdfb5249f29b5a814c4b7ee93067f5c26a950d
facundo2309 <facundoomargarcia@gmail.com>
2021-12-07 00:14:47 -0300

    css

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -2,14 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getCountriesById } from "../../actions";
+import style from './style.module.css'
 
 
 export default function Details({match}){
-    console.log(match.params.id)
+    //console.log(match.params.id)
 
     let [countryById]=useSelector(state=>state.countryById)
 
-    console.log(countryById)
+    //console.log(countryById)
     
 
     let dispatch=useDispatch()
@@ -21,36 +22,36 @@ export default function Details({match}){
     return(
         <div>
             <div>
-                <Link to='/home'><div>Home</div></Link>
+            <Link style={{ textDecoration: 'none' }} to='/home'><button className={style.buttonHome}>Home</button></Link>
             </div>
             <div>
-                <h1>{countryById?.name}</h1>
-                <img src={countryById?.image} alt='country flag'/>
-                <ul>
-                    <li >{countryById?.id}</li>
-                    <li  >{countryById?.continent}</li>
-                    <li  >{countryById?.capital}</li>
-                    <li >{countryById?.subregion}</li>
-                    <li >{countryById?.area}Km2</li>
-                    <li >{countryById?.population}</li>
+                <h1 className={style.title}>{countryById?.name}</h1>
+                <img src={countryById?.image} alt='country flag' width='250px' height='175px'/>
+                <div>
+                    <p className={style.text2}>{countryById?.id}</p>
+                    <p className={style.text2} >{countryById?.continent}</p>
+                    <p className={style.text2} >{countryById?.capital}</p>
+                    <p className={style.text2}>{countryById?.subregion}</p>
+                    <p className={style.text2}>Area: {countryById?.area} Km2</p>
+                    <p className={style.text2}>Population: {countryById?.population}</p>
                     
-                        <p>Activities</p>
+                        <p className={style.text}>Activities</p>
                         {countryById?.activities.length>0?
                         
                         countryById.activities.map((a,i)=>{return(
-                            <ul key={i+46772}>
-                                <li key={a.country_activity.activityUid+i+1}>Activity: {a.name}</li>
-                                <li key={a.country_activity.activityUid+i+2}>difficulty: {a.difficulty}</li>
-                                <li key={a.country_activity.activityUid+i+3}>duration: {a.duration}min</li>
-                                <li key={a.country_activity.activityUid+i+4}>season: {a.season}</li>
-                            </ul>
+                            <div className={style.ul} key={i+46772}>
+                                <p className={style.text3} key={a.country_activity.activityUid+i+1}>Activity: {a.name}</p>
+                                <p className={style.text3} key={a.country_activity.activityUid+i+2}>Difficulty: {a.difficulty}</p>
+                                <p className={style.text3} key={a.country_activity.activityUid+i+3}>Duration: {a.duration}min</p>
+                                <p className={style.text3} key={a.country_activity.activityUid+i+4}>Season: {a.season}</p>
+                            </div>
                             
                             
                         )})
 
-                        :<p>No activity added</p>}
+                        :<p className={style.description}>No activity added</p>}
                     
-                </ul>
+                </div>
             </div>
         </div>
     )
